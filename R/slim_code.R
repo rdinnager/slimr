472966a89d29b81630c331158a2e1156b17c88c2
Russell Dinnage <rdinnage@gmail.com>
2020-04-20 17:12:14 +1000

    added forgotten curly braces to fix slim error "all script blocks must be compound statements". Remember this one!

--- R/slim_code.R
+++ R/slim_code.R
@@ -8,7 +8,7 @@
 #' @examples
 slim_output_gens_code <- function(last_gen = NULL) {
 
-  code_bit <- glue::glue('catn("generation: " + paste(sim.generation));', .open = "<<", .close = ">>")
+  code_bit <- glue::glue('{catn("generation: " + paste(sim.generation));}', .open = "<<", .close = ">>")
   if(!is.null(last_gen)) {
     code_bit <- glue::glue("sim.registerLateEvent(NULL, '<<code_bit>>', start = 1, end = <<last_gen>>);",
                            .open = "<<", .close = ">>")

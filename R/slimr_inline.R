563629dcaffe001d03648f7ba35baff2eb015223
rdinnager <r.dinnage@gmail.com>
2020-07-08 13:41:54 +1000

    Added pkgdown website URL to DESCRIPTION

--- R/slimr_inline.R
+++ R/slimr_inline.R
@@ -1,3 +1,16 @@
 slimr_inline <- function(r_expr, unquote_strings = FALSE) {
 
 }
+
+#' Convert a string to a SLiM object name
+#'
+#' @param name A string with the SLiM object name
+#'
+#' @return A symbol
+#' @export
+#'
+#' @examples
+#' slimr_name("p1")
+slimr_name <- function(name) {
+  rlang::sym(name)
+}

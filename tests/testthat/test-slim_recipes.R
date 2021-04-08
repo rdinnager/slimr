1b4ce9b38884f2f624543a165af90fca5130d072
Russell.Dinnage <Russell.Dinnage@canberra.edu.au>
2021-04-08 16:27:16 +1000

    don't skip recipe conversion tests on ci

--- tests/testthat/test-slim_recipes.R
+++ tests/testthat/test-slim_recipes.R
@@ -2,9 +2,9 @@ test_that("All SLiM recipes can be converted to slimr_script objects properly", 
   slim_scripts <- list()
   slim_recipes <- slim_get_recipes()
 
-  skip_on_ci()
-  skip_on_cran()
-  skip_on_covr()
+  #skip_on_ci()
+  #skip_on_cran()
+  #skip_on_covr()
 
   for(i in seq_along(slim_recipes)) {
     #print(i)

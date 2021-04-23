e4cde2022933921d0231a731a733649af95306bc
Russell.Dinnage <Russell.Dinnage@canberra.edu.au>
2021-04-23 11:37:05 +1000

    updated WORDLIST and added more docos

--- R/slim_callbacks.R
+++ R/slim_callbacks.R
@@ -78,6 +78,40 @@ callbacks$recombination <- function(subpop_id) {
 
 }
 
+#' SLiM interaction() callback
+#'
+#' This callback specifies that a code block is providing logic to determine the
+#' strength of interaction between individuals. It should return the strength of the interaction
+#' as a numeric value. You must explicitly use \code{return(value)} at the end of the code block.
+#' For more information on how to use \code{interaction()} callback
+#' see \href{http://benhaller.com/slim/SLiM_Manual.pdf#page=604}{SLiM Manual: page 606}
+#'
+#' @details
+#' Global variables available in reproduction callbacks:
+#' \describe{
+#'   \item{distance}{The distance from receiver to exerter, in spatial simulations; NAN otherwise}
+#'   \item{strength}{The default interaction strength calculated by the interaction function}
+#'   \item{receiver}{The individual receiving the interaction (an object of class Individual)}
+#'   \item{exerter}{The individual exerting the interaction (an object of class Individual)}
+#'   \item{subpop}{The subpopulation in which the receiver and exerter live}
+#' }
+#'
+#' @param subpop_id The id(s) of the subpopulation(s) to which this callback should apply. Can
+#' be an integer 1, 2, etc., or character "p1", "p2", etc.
+#' @param int_type_id The id of the InteractionType to apply this callback to. Can
+#' be an integer 1, 2, etc., or character "i1", "i2", etc.
+#' @return None
+#' @export
+#'
+#' @examples
+#' slim_block(interaction(), {
+#'   # custom interaction strength as a function of values held in exerter and receiver tagF element.
+#'   return(dnorm(exerter.tagF, receiver.tagF, 0.1) / dnorm(0, 0, 0.1))
+#' })
+interaction <- function(int_type_id, subpop_id) {
+  callbacks$interaction(int_type_id, subpop_id)
+}
+
 callbacks$interaction <- function(int_type_id, subpop_id) {
 
   argus <- rlang::enexprs(int_type_id, subpop_id)
@@ -118,11 +152,11 @@ callbacks$interaction <- function(int_type_id, subpop_id) {
 #' @export
 #'
 #' @examples
-#' slim_block(reproduction()) {
+#' slim_block(reproduction(), {
 #'
 #'   subpop.addCrossed(individual, subpop.sampleIndividuals(1))
 #'
-#' }
+#' })
 reproduction <- function(subpop_id, sex) {
   callbacks$reproduction(subpop_id, sex)
 }

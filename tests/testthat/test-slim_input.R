47ad3c0d7c9ed7e8832957de2bc05bd856006031
rdinnager <r.dinnage@gmail.com>
2021-04-28 15:36:11 +1000

    fixing some tests

--- tests/testthat/test-slim_input.R
+++ tests/testthat/test-slim_input.R
@@ -1,4 +1,5 @@
 test_that("slim_make_pop_input works", {
+  set.seed(123)
   withr::local_file("pop.txt")
   pop_file <- slim_make_pop_input(matrix(rbinom(1000, 2, 0.25), nrow = 100, ncol = 100),
                                   file_name = "pop.txt")
